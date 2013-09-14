6fa9bdcea5766ff7862abb2cb078d22647798c0e
chris <chris.madden@gmail.com>
2013-09-13 23:05:41 -0400

    + make run

--- Makefile
+++ Makefile
@@ -3,21 +3,32 @@ STAMP_DEPS := $(STAMPS)/stamp-deps
 STAMP_COMPILE := $(STAMPS)/stamp-compile
 STAMP_GENERATE := $(STAMPS)/stamp-generate
 
-$(STAMP_DEPS):
+ALLFILES := $(shell find * -xdev \
+        -name .svn -prune \
+        -o -name '* *' -prune \
+        -o -path $(PLAT_TMP) -prune \
+        -o -name '*\#*' -prune \
+        -o -xtype f -print)
+
+ALLFILES := $(sort $(ALLFILES) $(ALL_GENERATED))
+ERL_SOURCE := $(filter apps/%.erl, $(ALLFILES))
+ERL_HEADER := $(filter apps/%.hrl, $(ALLFILES))
+
+$(STAMP_DEPS): rebar.config
 	rebar get-deps
 	@mkdir -p $(@D)
 	touch $@
 
 deps: $(STAMP_DEPS)
 
-$(STAMP_COMPILE): $(STAMP_DEPS)
+$(STAMP_COMPILE): $(STAMP_DEPS) $(ERL_SOURCE)
 	rebar compile
 	@mkdir -p $(@D)
 	touch $@
 
 compile: $(STAMP_COMPILE)
 
-$(STAMP_GENERATE): $(STAMP_COMPILE)
+$(STAMP_GENERATE): $(STAMP_COMPILE) rel/reltool.config
 	rebar generate
 	@mkdir -p $(@D)
 	touch $@
--- apps/th_core/src/th_core_sup.erl
+++ apps/th_core/src/th_core_sup.erl
@@ -1,28 +1,23 @@
-
 -module(th_core_sup).
+
+-include_lib("th_core/include/th_core.hrl").
 
 -behaviour(supervisor).
 
-%% API
 -export([start_link/0]).
 
-%% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
-
-%% ===================================================================
-%% API functions
-%% ===================================================================
+-define(CHILD(Id, Mod, Type, Args), {Id, {Mod, start_link, Args}, permanent, 5000, Type, [Mod]}).
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-%% ===================================================================
-%% Supervisor callbacks
-%% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+	?L_INFO("START"),
+	{ok,
+		{{one_for_one, 5, 10}, [
+		]}
+	}.
 
