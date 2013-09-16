bf7aa39cc61a15158ff91512b347db7ee40db50e
chris <chris.madden@gmail.com>
2013-09-16 09:14:29 -0400

    + moved

--- apps/th_core/include/th_core.hrl
+++ apps/th_core/include/th_core.hrl
@@ -2,16 +2,8 @@
 -define(__TH_CORE_HRL__, 1).
 
 -include_lib("lager/include/lager.hrl").
+-include_lib("thana_fw/include/thana_fw.hrl").
 
 -compile([ { parse_transform, lager_transform } ]).
--define(L_DEBUG, lager:debug).
--define(L_INFO,  lager:info).
--define(L_WARN,  lager:warning).
--define(L_ERROR, lager:error).
--define(L_ALERT, lager:alert).
-
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
--define(CHILD_ARGS(I, Type, Args), {I, {I, start_link, Args}, permanent, 5000, Type, [I]}).
--define(ID_CHILD_ARGS(I, M, Type, Args), {I, {M, start_link, Args}, permanent, 5000, Type, [M]}).
 
 -endif.
